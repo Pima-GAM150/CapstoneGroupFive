2893f6a4957cd6fe996c248dfee90852b5b775c2
TiPreble <tpreble@mail.pima.edu>
2019-04-29 23:29:09 -0700

    CameraCast Script Functional.  Bookshelf Prefab Created.
    CameraCast Script is now functioning.  If an object is set up properly, the script will disable the object's mesh renderer while that object is between the camera and the player character.

--- Assets/Scripts/CameraCastToCharacter.cs
+++ Assets/Scripts/CameraCastToCharacter.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CameraCastToCharacter : MonoBehaviour
 {
+    public GameObject MyCamera;
+    public GameObject Player;
+    MeshRenderer DisabledRenderer;
+    private List<MeshRenderer> myRenderList = new List<MeshRenderer>();
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,46 @@ public class CameraCastToCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        Physics.Linecast(transform.position, Player.transform.position, out RaycastHit hit);
+
+        if (hit.transform != null)
+        {
+            Debug.Log("Raycast successful.");
+            Debug.Log(hit.transform.name);
+        }
+
+        if (hit.transform.tag != "Player")
+        {
+            Debug.Log(hit.transform.name);
+            //if (hit.transform.GetComponent<MeshRenderer>() != DisabledRenderer)
+            if(myRenderList != hit.transform.gameObject.GetComponent<MeshRendererList>().thisObjectMeshes)
+            {
+                foreach (MeshRenderer enabledRender in myRenderList)
+                {
+                    enabledRender.enabled = true;
+                }
+                //DisabledRenderer.enabled = true;
+                //DisabledRenderer = hit.transform.GetComponent<MeshRenderer>();
+                myRenderList = hit.transform.gameObject.GetComponent<MeshRendererList>().thisObjectMeshes;
+                Debug.Log("Switching Disabled Objects");
+            }
+            //hit.transform.GetComponent<MeshRenderer>().enabled = false;
+            myRenderList = hit.transform.gameObject.GetComponent<MeshRendererList>().thisObjectMeshes;
+            foreach (MeshRenderer enabledRender in myRenderList)
+            {
+                enabledRender.enabled = false;
+            }
+            Debug.Log("Disabling Objects");
+        }
+        else
+        {
+            foreach (MeshRenderer enabledRender in myRenderList)
+            {
+                enabledRender.enabled = true;
+            }
+        }
     }
+
+    
 }
