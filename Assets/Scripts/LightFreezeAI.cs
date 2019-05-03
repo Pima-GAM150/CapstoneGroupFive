d3d6cf1e17831c1e078f3ced967e656f6a1a0440
Tim Preble <tpreble@mail.pima.edu>
2019-05-03 14:48:09 -0700

    Added monster to scene.  Got animation for monster working.

--- Assets/Scripts/LightFreezeAI.cs
+++ Assets/Scripts/LightFreezeAI.cs
@@ -6,6 +6,8 @@ public class LightFreezeAI : MonoBehaviour
 {
     public GameObject player;
 
+    public GameObject monster;
+
     private UnityEngine.AI.NavMeshAgent agent;
 
     public Collider LibraryBounds;
@@ -20,12 +22,15 @@ public class LightFreezeAI : MonoBehaviour
 
     private float chaseSpeed = 3;
 
+    private Animator myAnimator;
+
     private void Start()
     {
         getNewWaitTime();
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         agent.Warp(transform.position);
+        myAnimator = monster.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,14 +44,26 @@ public class LightFreezeAI : MonoBehaviour
             foreach (Light light in lightScan) { if (LibraryBounds.bounds.Contains(light.transform.position)) { agent.destination = transform.position; agent.speed = 0f; chase = false; }  }
             if (chase)
             {
+                myAnimator.SetBool("IsWalking", true);
                 agent.destination = player.transform.position;
                 agent.speed = chaseSpeed;
-                if (Vector3.Distance(transform.position, player.transform.position) < 2) player.GetComponentInChildren<PlayerHealth>().TakeDamage(5 * Time.deltaTime);
+                if (Vector3.Distance(transform.position, player.transform.position) < 2)
+                {
+                    player.GetComponentInChildren<PlayerHealth>().TakeDamage(5 * Time.deltaTime);                    
+                }
+            }
+            else
+            {
+                agent.destination = transform.position;
+                agent.speed = 0f;
+                myAnimator.SetBool("IsWalking", false);
             }
             extinguishWait -= Time.deltaTime;
-            if (extinguishWait <= 0) { MassExtinguish(); getNewWaitTime(); }
-        }
-        else { agent.destination = transform.position; agent.speed = 0f; }
+            if (extinguishWait <= 0)
+            {
+                MassExtinguish(); getNewWaitTime();
+            }
+        }    
     }
 
     private void getNewWaitTime() { extinguishWait = Random.Range(14f, 35f); }
